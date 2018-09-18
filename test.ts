e1672eb8284e4ffa26775b30746688f58842ee39
Venkata Kadiyala <vkadiyala@vmware.com>
2018-09-17 23:27:47 -0700

    first commit!

---
+++ test.ts
@@ -0,0 +1,18 @@
+var path = require("path");
+const git = require('simple-git/promise');
+
+var repo;
+var index;
+var oid;
+var repository;
+
+console.log('Cloning repo: ');
+let remote = `https://vkadiyala@vmware.com:VMware123@github.com/tangoe2eautomation/tango.git`;
+require('simple-git')()
+    .addConfig('user.name', 'Venkata Kadiyala')
+    .addConfig('user.email', 'vkadiyala@vmware.com');
+require('simple-git')()
+     .add('./*')
+     .commit("first commit!")
+     .push(['-u', 'origin', 'master'], () => console.log('done'));
+
